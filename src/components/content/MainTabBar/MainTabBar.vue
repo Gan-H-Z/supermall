0a12f798c9323335d05d6d6d7fa3da8e4a1720e3
itheima <itheima@itcast.cn>
2021-11-01 22:30:08 +0800

    提交

--- src/components/content/MainTabBar/MainTabBar.vue
+++ src/components/content/MainTabBar/MainTabBar.vue
@@ -4,33 +4,33 @@
       <tabbaritem path="/home" color="pink">
         <img
           slot="item-active"
-          src="assets/img/tabbar/home_active.svg"
+          src="~assets/img/tabbar/home_active.svg"
           alt=""
         />
-        <img slot="item-icon" src="assets/img/tabbar/home.svg" alt="" />
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt="" />
         <div slot="item-text">首页</div>
       </tabbaritem>
       <tabbaritem path="/category" color="pink">
-        <img slot="item-active" src="assets/img/tabbar/category_active.svg" />
-        <img slot="item-icon" src="assets/img/tabbar/category.svg" alt="" />
+        <img slot="item-active" src="~assets/img/tabbar/category_active.svg" />
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="" />
         <div slot="item-text">关于</div>
       </tabbaritem>
       <tabbaritem path="/cart" color="pink">
         <img
           slot="item-active"
-          src="assets/img/tabbar/shopcart_active.svg"
+          src="~assets/img/tabbar/cart_active.svg"
           alt=""
         />
-        <img slot="item-icon" src="assets/img/tabbar/shopcart.svg" alt="" />
+        <img slot="item-icon" src="~assets/img/tabbar/cart.svg" alt="" />
         <div slot="item-text">购物车</div>
       </tabbaritem>
       <tabbaritem path="/profile" color="pink">
         <img
           slot="item-active"
-          src="assets/img/tabbar/profile_active.svg"
+          src="~assets/img/tabbar/profile_active.svg"
           alt=""
         />
-        <img slot="item-icon" src="assets/img/tabbar/profile.svg" alt="" />
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="" />
         <div slot="item-text">我的</div>
       </tabbaritem>
     </tabbar>
