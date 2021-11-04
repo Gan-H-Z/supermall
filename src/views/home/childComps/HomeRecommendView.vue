83085d3dbde8975b03a5257ade6f0b3f5b8d69e4
itheima <itheima@itcast.cn>
2021-11-04 22:48:48 +0800

    提交

---
+++ src/views/home/childComps/HomeRecommendView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, index) in recommends" :key="index">
+      <a :href="item.link">
+        <img :src="item.image" alt="" />
+      </a>
+      <span>{{ item.title }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+.recommend {
+  width: 100%;
+  display: flex;
+  text-align: center;
+  font-size: 12px;
+  border-bottom: 8px solid rgb(241, 241, 241);
+  padding: 7px 0 20px;
+}
+.recommend div {
+  flex: 1;
+}
+.recommend img {
+  width: 70px;
+  height: 70px;
+  margin-bottom: 3px;
+}
+</style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,50 @@
 <template>
   <div id="home">
     <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"></home-swiper>
+    <home-swiper :banners="banners" class="swiper"></home-swiper>
+    <home-recommend :recommends="recommends" />
+    <home-feature />
+
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+    </ul>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper.vue";
+import HomeRecommend from "./childComps/HomeRecommendView.vue";
+import HomeFeature from "./childComps/HomeFeatureView.vue";
 
 import { getHomeMultidata } from "network/home.js";
 
@@ -15,6 +52,8 @@ export default {
   components: {
     NavBar,
     HomeSwiper,
+    HomeRecommend,
+    HomeFeature,
   },
   data() {
     return {
@@ -46,5 +85,13 @@ export default {
 .nav-bar {
   background-color: rgb(240, 161, 174);
   color: #fff;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
+.swiper {
+  padding-top: 44px;
 }
 </style>
