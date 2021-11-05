5a5f28a363ad4589478e5e3732dab2c47d1de6ce
itheima <itheima@itcast.cn>
2021-11-05 22:32:15 +0800

    提交

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item, index) in control"
+      class="tab-control-item"
+      :key="index"
+    ></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    control: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
